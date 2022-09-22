b4ca9696c47a79af1b6190ba967651b3c7f8cc19
vladzi-o <vladzio7770@gmail.com>
2022-09-22 23:25:27 +0300

    finish dz24.3

--- dz25/script.js
+++ dz25/script.js
@@ -1,4 +1,4 @@
-const API_URL = "https://63104dd6826b98071a3d9b42.mockapi.io";
+const API_URL = "https://632a220b1090510116bbe8cd.mockapi.io";
 const HEADERS = {
   "content-type": "application/json",
 };
@@ -37,36 +37,51 @@ function renderHero(hero) {
   });
 }
 
-function fetchHeroes() {
-  fetch(`${API_URL}/heroes`, { headers: HEADERS })
-    .then((response) => response.json())
-    .then((data) => {
-      data.forEach(renderHero);
-    });
+function renderUniverse(name) {
+  const newOption = document.createElement("option");
+  newOption.value = name;
+  newOption.innerHTML = name;
+  universeSelect.appendChild(newOption);
 }
 
-function addHero(hero) {
-  return fetch(`${API_URL}/heroes`, {
+async function fetchResourse(resourse, options = {}) {
+  const response = await fetch(`${API_URL}/${resourse}`, {
+    headers: HEADERS,
+    ...options,
+  });
+  const data = await response.json();
+  return data;
+}
+
+async function fetchHeroes() {
+  const heroes = await fetchResourse("heroes");
+  heroes.forEach(renderHero);
+}
+
+async function fetchUniverses() {
+  const universes = await fetchResourse("universes");
+  universes.forEach((universe) => renderUniverse(universe.name));
+}
+
+async function addHero(hero) {
+  const newHero = await fetchResourse("heroes", {
     method: "POST",
     body: JSON.stringify(hero),
-    headers: HEADERS,
   });
+  return newHero;
 }
 
 async function deleteHero(hero, row) {
-  await fetch(`${API_URL}/heroes/${hero.id}`, {
-    method: "DELETE",
-    headers: HEADERS,
-  });
+  await fetchResourse(`heroes/${hero.id}`, { method: "DELETE" });
   row.remove();
 }
 
 form.addEventListener("submit", async (event) => {
   event.preventDefault();
   const heroFormData = getFormData();
-  const response = await addHero(heroFormData);
-  const hero = await response.json();
+  const hero = await addHero(heroFormData);
   renderHero(hero);
 });
 
+fetchUniverses();
 fetchHeroes();
